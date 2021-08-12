6426fe2289e4541e2c98c2f49e67dcb26cd6ca4d
liteng <2212295868@QQ.com>
2021-08-12 22:54:08 +0800

    完成个人中心

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,61 @@
+// 设置表单的输入规则
+$(function() {
+    var form = layui.form
+        // 实现提示错误消息的功能
+    var layer = layui.layer
+
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1~6 个字符之间!'
+            }
+        }
+    })
+
+    initUserInfo()
+
+    // 初始化，获取到用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                // console.log(res)
+                // 调用form.value()，快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    // 重置表单的数据
+    $('#btnReset').on('click', function(e) {
+        // 阻止表单的默认重置行为
+        e.preventDefault()
+        initUserInfo()
+    })
+
+    // j监听表单提交
+    $('.layui-form').on('submit', function(e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+            // 发起ajax数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            // $(this)就是当前表单，serialize()用来那数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+                    // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+                window.parent.getUserInfo()
+            }
+        })
+    })
+
+})
